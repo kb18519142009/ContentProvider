b26e4db0b85e2596c0a9118876b8bc7df93e7c2c
kangbaibai <kangbaibai@btime.com>
2018-10-17 09:56:01 +0800

    整理

--- app/src/main/java/com/example/kangbaibai/bookprovider/MainActivity.java
+++ app/src/main/java/com/example/kangbaibai/bookprovider/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 //        Uri uri = Uri.parse("content://com.example.kangbaibai.bookprovider.BookProvider");
 //        getContentResolver().query(uri, null, null, null, null);
 //        getContentResolver().query(uri, null, null, null, null);
